8e8c6e3695dbdc8993fdce4246c6dc3a8478aac6
newxata <9119355@gmail.com>
2026-05-27 11:13:50 +0300

    Upgrade func show_all

--- hw4_4.py
+++ hw4_4.py
@@ -1,48 +1,52 @@
 # Функція яка приймає рядок вводу користувача
 def parse_input(user_input):
     # Повертаємо перше слово як команду та зберігаємо у змінній cmd, решту зберігаємо як список аргументів *args
-    cmd, *args = user_input.split()
+    cmd, *args = user_input.split(' ')
     # Видаляємо зайві пробіли та перетворюємо на нижній регістр
     cmd = cmd.strip().lower()
-    return cmd, *args
+    return cmd, args
 
 # Функція додавання нового контакту до словника контактів
 def add_contact(args, contacts):
-    # Обмеження на введення більше значень ніж name та phone
+    # Обмеження на введення більше значень ніж двох значень
     if len(args) == 2:
         name, phone = args
         contacts[name] = phone
         return 'Contact added.'
     else:
-        return 'Error. Enter name and phone.'
+        return 'Enter name and phone.'
 
-# Функція зміни номеру телефону для контакту який вже існує в списку
+# Функція зміни номера телефону для контакту який вже існує в списку
 def change_contact(args, contacts):
+    # Обмеження на введення більше значень ніж двох значень
     if len(args) == 2:
         name, phone = args
-        # Умова якщо імʼя відсутнє в списку
-        if name in contacts:
-            contacts[name] = phone
-            return 'Contact updated.'
-        else:
-            return 'No contact.'
+        # Перевірка існування контакту в списку
+        if name not in contacts:
+            return 'Contact not found.'
+        contacts[name] = phone
+        return 'Contact updated.'
     else:
-        return 'Error. Enter name and phone.'
+        return 'Enter name and phone.'
 
-# Функція виведення номеру телефону
+# Функція виведення номера телефону
 def show_phone(args, contacts):
+    # Обмеження на введення більше значень ніж одного значення
     if len(args) == 1:
         name = args[0]
-        if name in contacts:
-            return contacts[name]
-        else:
-            return 'No contact.'
+        # Перевірка існування контакту в списку
+        if name not in contacts:
+            return 'Contact not found.'
+        return contacts[name]
     else:
-        return 'Error. Enter name.'
+        return 'Enter the name.'
 
 # Функція виведення усіх збережених контактів
-def show_all(contacts):
-    return contacts
+def show_all(args, contacts):
+    if len(args) == 0:
+        return '\n'.join(f'{name}: {phone}' for name, phone in contacts.items())
+    else:
+        return 'Enter only "all".'
 
 def main():
     # Створюємо словник контактів
@@ -50,14 +54,15 @@ def main():
     print('Welcome to the assistant bot!')
     while True:
         user_input = input('Enter a command: ')
-        # Змінна command отримує перше введене слово та стає командою, а змінна *args списком з усіх інших значень
-        command, *args = parse_input(user_input)
+        # Змінна command отримує перше введене слово та стає командою, а змінна args списком з усіх інших значень
+        command, args = parse_input(user_input)
 
-        # Якщо команда close або exit то зупиняємо цикл
+        # Команда close або exit зупиняємо цикл і виходить з програми
         if command in ['close', 'exit']:
             print('Good bye!')
             break
 
+        # Команди програми
         elif command == 'hello':
             print('How can I help you?')
         elif command == 'add':
@@ -67,7 +72,7 @@ def main():
         elif command == 'phone':
             print(show_phone(args, contacts))
         elif command == 'all':
-            print(show_all(contacts))
+            print(show_all(args, contacts))
         else:
             print('Invalid command.')
 
